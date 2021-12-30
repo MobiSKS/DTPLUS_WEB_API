cfb0d9cdc1f25fa8b66190667235a3fb35086694
rajeev <rajeev@paytmmloyal.com>
2021-12-30 21:41:49 +0530

    Add New Method

--- HPCL_WebApi/Controllers/AccountController.cs
+++ HPCL_WebApi/Controllers/AccountController.cs
@@ -9,6 +9,10 @@ using static HPCL.Infrastructure.CommonClass.StatusMessage;
 using HPCL.Infrastructure.TokenManager;
 using Microsoft.Extensions.Configuration;
 using HPCL_WebApi.ExtensionMethod;
+using Microsoft.AspNetCore.Http;
+using HPCL.Infrastructure.Extension;
+using HPCL.DataModel.Account;
+
 namespace HPCL_WebApi.Controllers
 {
 
@@ -26,6 +30,168 @@ namespace HPCL_WebApi.Controllers
             ObjVariable = new Variables(configuration);
         }
 
+        private  bool Return_Key(IAccountRepository accountRepo,
+          out string UserMessage, int Header_Parameter_Value, out int Status_Code, string Useragent, string Userip, string Method_Name, string Userid)
+        {
+            var request = Request;
+            var headers = request.Headers;
+            bool IsResult = false;
+            string Secret_Key = string.Empty;
+            string StrMessage = string.Empty;
+            int IntStatusCode = 0;
+            try
+            {
+                string API_Key;
+                if (Header_Parameter_Value == 0)
+                {
+                    API_Key = request.GetHeader("API_Key");
+                    Secret_Key = request.GetHeader("Secret_Key");
+
+                    if (API_Key == "" && Secret_Key != "")
+                    {
+                        StrMessage = StatusInformation.API_Key_Is_Null.GetDisplayName();
+                        IntStatusCode = (int)StatusInformation.API_Key_Is_Null;
+                        IsResult = false;
+                    }
+                    else if (API_Key != "" && Secret_Key == "")
+                    {
+                        StrMessage = StatusInformation.Secret_Key_Is_Null.GetDisplayName();
+                        IntStatusCode = (int)StatusInformation.Secret_Key_Is_Null;
+                        IsResult = false;
+                    }
+
+                    else if (API_Key == "" && Secret_Key == "")
+                    {
+                        StrMessage = StatusInformation.API_Key_Secret_Key_Is_Null.GetDisplayName();
+                        IntStatusCode = (int)StatusInformation.API_Key_Secret_Key_Is_Null;
+                        IsResult = false;
+                    }
+                    else
+                    {
+                        bool IsAPI_Key_Validate = ObjVariable.Chk_API_Key_SecretKey_Validation(API_Key, Secret_Key);
+                        if (IsAPI_Key_Validate == true)
+                        {
+                            IsResult = true;
+                            StrMessage = StatusInformation.Success.GetDisplayName();
+                            IntStatusCode = (int)StatusInformation.Success;
+                        }
+
+                        else
+                        {
+                            IsResult = false;
+                            StrMessage = StatusInformation.API_Key_Is_Secret_Key_Invalid.GetDisplayName();
+                            IntStatusCode = (int)StatusInformation.API_Key_Is_Secret_Key_Invalid;
+                        }
+                    }
+
+
+                }
+
+                if (Header_Parameter_Value == 1)
+                {
+                    API_Key = request.GetHeader("API_Key");
+
+                    if (API_Key == "")
+                    {
+                        StrMessage = StatusInformation.API_Key_Is_Null.GetDisplayName();
+                        IntStatusCode = (int)StatusInformation.API_Key_Is_Null;
+                        IsResult = false;
+                    }
+                    else
+                    {
+                        bool IsAPI_Key_Validate = ObjVariable.Chk_APIKey_Validation(API_Key);
+                        if (IsAPI_Key_Validate == true)
+                        {
+                            IsResult = true;
+                            StrMessage = StatusInformation.Success.GetDisplayName();
+                            IntStatusCode = (int)StatusInformation.Success;
+                        }
+                        else
+                        {
+                            IsResult = false;
+                            StrMessage = StatusInformation.API_Key_Is_Invalid.GetDisplayName();
+                            IntStatusCode = (int)StatusInformation.API_Key_Is_Invalid;
+                        }
+
+                    }
+
+
+                }
+
+                if (Header_Parameter_Value == 2)
+                {
+
+                    API_Key = request.GetHeader("Secret_Key");
+                    if (Secret_Key == "")
+                    {
+                        StrMessage = StatusInformation.Secret_Key_Is_Null.GetDisplayName();
+                        IntStatusCode = (int)StatusInformation.Secret_Key_Is_Null;
+                        IsResult = false;
+                    }
+                    else
+                    {
+                        bool IsSecret_Key_Validate = ObjVariable.Chk_SecretKey_Validation(Secret_Key);
+                        if (IsSecret_Key_Validate == true)
+                        {
+                            StrMessage = StatusInformation.Success.GetDisplayName();
+                            IntStatusCode = (int)StatusInformation.Success;
+                            IsResult = true;
+                        }
+                        else
+                        {
+                            StrMessage = StatusInformation.Secret_Key_Is_Invalid.GetDisplayName();
+                            IntStatusCode = (int)StatusInformation.Secret_Key_Is_Invalid;
+                            IsResult = false;
+                        }
+
+                    }
+                }
+            }
+            catch
+            {
+                StrMessage = StatusInformation.Internel_Error.GetDisplayName();
+                IntStatusCode = (int)StatusInformation.Internel_Error;
+                IsResult = false;
+            }
+
+
+            if (IsResult == true)
+            {
+                if (Useragent == null)
+                    Useragent = "";
+
+                if (Userip == null)
+                    Userip = "";
+
+                if (Userid == null)
+                    Userid = "";
+
+                if (Userid == "")
+                    Userid = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+                if (Useragent != "" && Userip != "" && Userid != "")
+                {
+                    AccountModel objAccountModel = new AccountModel
+                    {
+                        MethodName = Method_Name,
+                        Useragent = Useragent,
+                        Userid = Userid,
+                        Userip = Userip
+                    };
+                    IsResult = true;
+                }
+                else
+                {
+                    IsResult = false;
+                    StrMessage = StatusInformation.User_Agentor_User_IP_User_Id_is_null.GetDisplayName();
+                    IntStatusCode = (int)StatusInformation.User_Agentor_User_IP_User_Id_is_null;
+                }
+            }
+
+            UserMessage = StrMessage;
+            Status_Code = IntStatusCode;
+            return IsResult;
+        }
 
 
         [HttpPost]
@@ -43,7 +209,7 @@ namespace HPCL_WebApi.Controllers
                 string Secret_Key = string.Empty;
                 byte[] bytes = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70 };
                 string SecretKey = Convert.ToBase64String(bytes);
-                bool IsResult = TokenManager.Return_Key(Request, ObjVariable, _accountRepo, out string UserMessage, 0, out int IntStatusCode, ObjClass.Useragent, ObjClass.Userip, MethodName, ObjClass.Userid);
+                bool IsResult = this.Return_Key( _accountRepo, out string UserMessage, 0, out int IntStatusCode, ObjClass.Useragent, ObjClass.Userip, MethodName, ObjClass.Userid);
 
                 if (IsResult == true)
                 {
