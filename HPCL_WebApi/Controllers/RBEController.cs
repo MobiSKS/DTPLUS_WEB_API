ea2f3631e7d53098982232645da8dddde5f54aad
amitpaytmmloyal <amit@paytmmloyal.com>
2022-04-19 19:31:59 +0530

    commit rbe

--- HPCL_WebApi/Controllers/RBEController.cs
+++ HPCL_WebApi/Controllers/RBEController.cs
@@ -657,5 +657,32 @@ namespace HPCL_WebApi.Controllers
                 }
             }
         }
+
+        [HttpPost]
+        [ServiceFilter(typeof(CustomAuthenticationFilter))]
+        [Route("Validate_Otp_RBE_Mapping")]
+        public async Task<IActionResult> ValidateOtpRBEMapping([FromBody] ValidateOtpRBEMappingModelInput ObjClass)
+        {
+            if (ObjClass == null)
+            {
+                return this.BadRequestCustom(ObjClass, null, _logger);
+            }
+            else
+            {
+                var result = await _RBERepo.ValidateOtpRBEMapping(ObjClass);
+                if (result == null)
+                {
+                    return this.NotFoundCustom(ObjClass, null, _logger);
+                }
+                else
+                {
+                    List<ValidateOtpRBEMappingModelOutput> item = result.Cast<ValidateOtpRBEMappingModelOutput>().ToList();
+                    if (item.Count > 0)
+                        return this.OkCustom(ObjClass, result, _logger);
+                    else
+                        return this.Fail(ObjClass, result, _logger);
+                }
+            }
+        }
     }
 }
