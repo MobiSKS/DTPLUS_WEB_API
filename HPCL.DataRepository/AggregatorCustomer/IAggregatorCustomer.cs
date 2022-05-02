2ba4a1dd5627ff1e128e08eaaeeb5326badfdebb
amitpaytmmloyal <amit@paytmmloyal.com>
2022-05-02 16:14:12 +0530

    tms

---
+++ HPCL.DataRepository/AggregatorCustomer/IAggregatorCustomer.cs
@@ -0,0 +1,16 @@
+﻿using HPCL.DataModel.AggregatorCustomer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace HPCL.DataRepository.AggregatorCustomer
+{
+    public interface IAggregatorCustomerRepository
+    {
+        public Task<IEnumerable<AggregatorCustomerInsertModelOutput>> InsertAggregatorCustomer([FromBody] AggregatorCustomerInsertModelInput ObjClass);
+
+    }
+}
