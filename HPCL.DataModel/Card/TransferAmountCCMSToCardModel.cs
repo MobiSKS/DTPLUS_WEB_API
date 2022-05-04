f6fc97cdc04c485b8b0668a82c424d57998f90df
manmohanverma <manmohan@paytmmloyal>
2022-05-04 11:45:00 +0530

    changes

--- HPCL.DataModel/Card/TransferAmountCCMSToCardModel.cs
+++ HPCL.DataModel/Card/TransferAmountCCMSToCardModel.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HPCL.DataModel.Card
 {
     public class TransferAmountCCMSToCardModelInput:BaseClass
     {
+        [Required]
+        [JsonPropertyName("CustomerId")]
+        [DataMember]
         public string CustomerId { get; set; }
         public List<CCMSToCardTransfer> CCMSToCardTransfer { get; set; }
 
