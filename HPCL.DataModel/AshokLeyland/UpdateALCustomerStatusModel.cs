6e431b73d583b649a2ba3d6b19d2bc75542a488e
manmohanverma <manmohan@paytmmloyal>
2022-05-06 13:09:59 +0530

    AL Module API Added

---
+++ HPCL.DataModel/AshokLeyland/UpdateALCustomerStatusModel.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCL.DataModel.AshokLeyland
+{
+    public class UpdateALCustomerStatusModelInput:BaseClass
+    {
+        [Required]
+        [JsonProperty("CustomerID")]
+        [DataMember]
+        public int CustomerID { get; set; }
+
+        [Required]
+        [JsonProperty("CustomerStatus")]
+        [DataMember]
+        public int CustomerStatus { get; set; }
+       
+        
+        
+    }
+
+    public class UpdateALCustomerStatusModelOutput : BaseClassOutput
+    {
+    }
+}
